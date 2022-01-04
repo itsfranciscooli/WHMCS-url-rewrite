d7d6c3e2b4205610a4403962ddfd66e052b07dcb
Francisco Oliveira <75614393+itsfranciscooli@users.noreply.github.com>
2022-01-04 02:15:08 +0000

    Add files via upload

---
+++ link.php
@@ -0,0 +1,24 @@
+<?php
+
+use WHMCS\View\Menu\Item as MenuItem;
+
+add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
+{
+   if (!is_null($primaryNavbar->getChild('Knowledgebase'))) {
+       $primaryNavbar->getChild('Knowledgebase')
+                       ->setURI('https://help.xmhosts.com');
+   }
+
+   if (!is_null($primaryNavbar->getChild('Support'))) {
+       $primaryNavbar->getChild('Support')
+                       ->getChild('Knowledgebase')
+                       ->setURI('https://help.xmhosts.com');
+   }
+   
+   if (!is_null($primaryNavbar->getChild('Network Status'))) {
+    $primaryNavbar->getChild('Network Status')
+                    ->setURI('https://status.xmhosts.com');
+   
+   
+}     
+});
