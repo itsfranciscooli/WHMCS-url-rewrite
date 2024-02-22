c5361ece0287a3b902aac30e7dc49b068c59e516
Francisco Oliveira <75614393+itsfranciscooli@users.noreply.github.com>
2024-02-22 19:14:28 +0000

    Update link.php

--- link.php
+++ link.php
@@ -1,17 +1,4 @@
 <?php
-
-use WHMCS\View\Menu\Item as MenuItem;
-
-add_hook('ClientAreaPrimaryNavbar', 1, function (MenuItem $primaryNavbar)
-{
-   if (!is_null($primaryNavbar->getChild('Knowledgebase'))) {
-       $primaryNavbar->getChild('Knowledgebase')
-                       ->setURI('https://help.xmhosts.com');
-   }
-
-   if (!is_null($primaryNavbar->getChild('Support'))) {
-       $primaryNavbar->getChild('Support')
-                       ->getChild('Knowledgebase')
-                       ->setURI('https://help.xmhosts.com');
-   }
+add_hook('ClientAreaPageServerStatus', 1, function($vars) {
+    header("Location: https://status.franciscooliveira.me");
 });
