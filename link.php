5ccf5f6a3a12d0da870d4e4783fd005278ea8539
Francisco Oliveira <75614393+itsfranciscooli@users.noreply.github.com>
2024-09-20 23:57:37 +0100

    Update link.php
    Now includes knowledgebase, announcements and network status

--- link.php
+++ link.php
@@ -1,4 +1,18 @@
 <?php
+// Redirect the Network Status page
 add_hook('ClientAreaPageServerStatus', 1, function($vars) {
-    header("Location: https://status.franciscooliveira.me");
+    header("Location: https://status.tenzix.com");
+    exit;
+});
+
+// Redirect the Announcements page
+add_hook('ClientAreaPageAnnouncements', 1, function($vars) {
+    header("Location: https://blog.tenzix.com");
+    exit;
+});
+
+// Redirect the Knowledgebase page
+add_hook('ClientAreaPageKnowledgebase', 1, function($vars) {
+    header("Location: https://help.tenzix.com");
+    exit; 
 });
